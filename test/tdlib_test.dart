ec365f95612cdaf219e05f1669226de37ec576a8
i-Naji <teamnaji@gmail.com>
2020-08-08 18:50:10 +0430

    need new test

--- test/tdlib_test.dart
+++ test/tdlib_test.dart
@@ -1,21 +1,4 @@
-import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:tdlib/td_api.dart' as TdApi;
+import 'package:flutter/services.dart'; // ignore: unused_import
+import 'package:flutter_test/flutter_test.dart'; // ignore: unused_import
 
-void main() {
-  const MethodChannel channel = MethodChannel('tdlib');
-
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
-  });
-
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(TdApi.TestGetDifference(), '42');
-  });
-}
+void main() {}
