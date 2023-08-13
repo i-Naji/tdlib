4c26d6c504c806e062d3a3dd6f57f2a4d3dd6b10
naji <teamnaji@gmail.com>
2023-08-13 22:35:50 +0330

    fix gradle error

---
+++ android/src/main/java/org/naji/td/tdlib/TdlibPlugin.java
@@ -0,0 +1,19 @@
+package org.naji.td.tdlib;
+
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+
+/** TdlibPlugin */
+public class TdlibPlugin implements FlutterPlugin {
+
+
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+  }
+
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+  }
+}
